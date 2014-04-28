b7afe05d451367915cd87091903efb1d94a1756a
citybeams <nicklohr1@gmail.com>
2014-04-28 15:04:02 -0400

    can add and view posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -13,4 +26,11 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
