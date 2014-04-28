71ab9e0b48af405a84a0c9355ec0c18ba3576d5d
citybeams <nicklohr1@gmail.com>
2014-04-28 19:01:29 -0400

    can respond to posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,15 @@ class CommentsController < ApplicationController
 
 	# I'm omitting the index method, since I don't think we wanna ever see all the comments on all posts listed in
 	# one place
-	
+
 	def new
 	end
 
 	def create
-	end
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
 
 	def edit
 	end
@@ -17,5 +20,11 @@ class CommentsController < ApplicationController
 
 	def destroy
 	end
+
+	 private
+
+   def comment_params
+      params.require(:comment).permit(:text)
+   end
 
 end
