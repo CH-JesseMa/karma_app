93060f571b106aad4f277773f6fa64ce86bc05ef
ghostropic <jon.bichar@gmail.com>
2014-04-29 19:24:45 -0400

    deleted user_session again

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,5 +1,16 @@
 var postsPage = {
 
+
+  onReady: function() {
+  $(".posts").empty();
+  $("<button>").
+  }
+
 };
 
 $(document).ready(postPage.onReady);
+
+
+if post.post_type = "I'm offering services"
+   button1
+else
