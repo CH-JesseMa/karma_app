35a6276dd82ee6a401641c209df02549b8b9ebd2
generationloss <hwilliamstites@gmail.com>
2014-04-28 20:47:34 -0400

    fixed posts/destroy path

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,7 +18,7 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments, dependent: :destroy
-  validates :title, presence: true,
+  	validates :title, presence: true,
                     length: { minimum: 5 }
 end
 
