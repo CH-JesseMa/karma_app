cf09665e0773f933c834ff6b63f87f26573c4db0
ghostropic <jon.bichar@gmail.com>
2014-04-30 14:05:39 -0400

    added js functionality to posts index

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,11 +1,23 @@
 var postsPage = {
 
 
-  onReady: function() {
-  $(".posts").empty();
+  onReady: function() { postsPage.postButtons() },
+
+  postButtons: function() {
+    $("#requests").hide();
+    $("#offers").hide();
+    $("#show_requests").on("click", function() {
+      $("#offers").hide();
+      $("#requests").show();
+    });
+
+    $("#show_offers").on("click", function() {
+      $("#requests").hide();
+      $("#offers").show();
+    });
   }
 
-};
 
-$(document).ready(postsPage.onReady());
+} //postsPage
 
+$(document).ready(postsPage.onReady);
