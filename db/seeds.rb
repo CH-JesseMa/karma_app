2812b22cef357cac66fa7d647545b66f3ca03862
CH-JesseMa <jesse@casthaven.com>
2014-05-01 13:00:03 -0400

    add user index html table and commented out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,14 @@ User.destroy_all
 Post.destroy_all
 Transaction.destroy_all
 
-bill = User.create(name: "Bill", email: "bill@bill.com", password: "abcd1234", password_confirmation: "abcd1234", points: 10, user_name: "generationloss")
-jon = User.create(name: "Jon", email: "jon@jon.com", password: "abcd1234", password_confirmation: "abcd1234", points: 10, user_name: "Bichar")
-admin = User.create(name: "admin", email: "admin@admin.com", password: "alligator", password_confirmation: "alligator", is_admin: true, user_name: "admin")
+# bill = User.create(name: "Bill", email: "bill@bill.com", password: "abcd1234", password_confirmation: "abcd1234", points: 10, user_name: "generationloss")
+# jon = User.create(name: "Jon", email: "jon@jon.com", password: "abcd1234", password_confirmation: "abcd1234", points: 10, user_name: "Bichar")
+# admin = User.create(name: "admin", email: "admin@admin.com", password: "alligator", password_confirmation: "alligator", is_admin: true, user_name: "admin")
 
-poast = Post.create(karma_value: 5, is_open: true, post_type: "offer", title: "Hey how's it goin", content: "Hey how's it goin?", user_id: bill.id)
-Post.create(karma_value: 2, is_open: true, post_type: "request", title: "Are you Sancho?", content: "No, you are not Sancho.", user_id: jon.id)
+# poast = Post.create(karma_value: 5, is_open: true, post_type: "offer", title: "Hey how's it goin", content: "Hey how's it goin?", user_id: bill.id)
+# Post.create(karma_value: 2, is_open: true, post_type: "request", title: "Are you Sancho?", content: "No, you are not Sancho.", user_id: jon.id)
 
 
-Transaction.create(requester_id: jon.id, servicer_id: bill.id, requester_complete: false, servicer_complete: false, is_complete: false, point_value: poast.karma_value, post_id: poast.id)
-Transaction.create(requester_id: bill.id, servicer_id: jon.id)
+# Transaction.create(requester_id: jon.id, servicer_id: bill.id, requester_complete: false, servicer_complete: false, is_complete: false, point_value: poast.karma_value, post_id: poast.id)
+# Transaction.create(requester_id: bill.id, servicer_id: jon.id)
 
