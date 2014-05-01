ec92b8c264ce36ccc48f544d98fc98c46e6c99f4
generationloss <hwilliamstites@gmail.com>
2014-05-01 15:56:17 -0400

    I don't fuckin know anymore

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,11 @@
 class WelcomeController < ApplicationController
 
 	def index
-
-    if logged_in?
-      @transactions = current_user.generate_transactions_array
-      @local_posts = current_user.find_local_posts
-      @open_posts = current_user.return_open_posts
-    end
-
-
+	    if logged_in?
+	      @transactions = current_user.generate_transactions_array
+	      @local_posts = current_user.find_local_posts
+	      @open_posts = current_user.return_open_posts
+	    end
 	end
 
 end
