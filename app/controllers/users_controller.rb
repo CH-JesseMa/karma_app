f0cb0fca96a01f1b284b08d8056cbb09a5e6d44f
generationloss <hwilliamstites@gmail.com>
2014-05-01 13:59:55 -0400

    creating user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user = User.find_by(id: params[:id])
   end
 
   # POST /users
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 
 	def index
+		@transactions = current_user.generate_transactions_array
 	end
 	
 end
